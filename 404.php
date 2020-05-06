e49fb9306646a65d16d7d910a234e7af6077a429
Rafael Raszl <rafaelmsraszl@gmail.com>
2020-05-05 23:57:13 -0300

    Alterado página 404

--- 404.php
+++ 404.php
@@ -1,16 +1,24 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
+<!DOCTYPE HTML>
+<html lang="pt-br">
     <head>
-	<title>404</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<title>404 Error</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <script src="app/js/jquery/jquery-1.8.3.min.js" type="text/javascript"></script>
+    <script src="app/js/jquery/jquery-ui-1.9.2.js" type="text/javascript"></script>
+    <script src="app/js/jquery/bootstrap/bootstrap.js" type="text/javascript"></script>
+    <link href="app/js/jquery/bootstrap/bootstrap.css" rel="stylesheet" />
     </head>
     <body>
-    <title>Ooops</title>
     <br /><br /><br />
     <center>
-	<h1>404! Ooops</h1>
-	<img src="app/images/default/404.jpg" />
-	<h1>Parece que a página não existe!</h1>
+	<!-- <h1>404! Ooops</h1> -->
+    <div class="container-fluid text-center">
+	   <img class="rounded img-fluid" src="app/images/default/404.jpg" style="max-width: 100%; height: auto;"/>
+    </div>
+	<!-- <h1>Parece que a página não existe!</h1> -->
     </center>
+
+
+    <script src="app/js/public/main.js" type="text/javascript"></script>
 </body>
 </html>
